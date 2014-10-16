3946c92274970b156c5ec6c4a03435af9fab1842
Leta Montopoli <lmontopo@gmail.com>
2014-10-16 13:15:22 -0400

    Decided on a slightly new algorithm. Added a working parser. Not working as a whole - need to amend our previous code.

--- lisp_interpreter.py
+++ lisp_interpreter.py
@@ -1,15 +1,67 @@
-#holder = "(/ (+ 3  ( * 2 3 )) (- -4 2))"
-holder = '(define x 3)(* (+ 3 2) x)'
+holder = "(/ (+ 3  ( * 2 3 )) (- -4 2))"
+#holder = '(define x 3)(* (+ 3 2) x)'
 
-holder = holder.replace( '(' , ' ( ' )
-holder = holder.replace( ')', ' ) ' )
+holder = holder.replace( '(' , ' [ ' )
+holder = holder.replace( ')', ' ] ' )
 
 holder = holder.split(' ')
 holder = filter(lambda a: a != '', holder)
-print holder
+print holder, 'before parsing'
+#def parse(list):
+#	if not list: 
+#		return
+#		raise ValueError('function being wierd')
+#	new_holder = []
+#	love = 0 
+#	for i in range(0,len(list)):
+#		if holder[i] == '[':
+#			love +=1
+#		elif holder[i] == ']':
+#			love -=1
+#		if love ==  0 :
+#			new_holder.append(list[i])
+#		elif love > 0:
+#			while love > 0:
+#				parse(new_holder)
+#	return new_holder		
+		
+#print parse(holder)
 
+
+
+def parse(holder):
+	if len(holder) == 0:
+		raise SyntaxError('unexpectedly entered function')
+	
+	token = holder[0]
+	holder = holder[1:]
+
+	if token == '[':
+		new_holder = [] #sets new holder as an array
+		
+		while holder[0] != ']':
+			to_append, holder = parse(holder)
+			new_holder.append(to_append)
+		holder = holder[1:]			 #pops off the ')' part
+		
+		#we add a condition to check if last return
+		if len(holder) > 0:
+			return new_holder , holder #if not last return, need to return current version of holder
+		elif len(holder) == 0 :
+			return new_holder			#if last return, only want to return new_holder
+	else:
+		return token, holder 			#this is never last return, so we always need both
+
+		
+print parse(holder)
+
+
+	
+
+# a dictionary where we will add any variable declarations
 var_dict = { }
 
+#checks if our operands are numbers
 def is_number(x):
 	try:
 		float(x)
@@ -17,15 +69,17 @@ def is_number(x):
 	except ValueError:
 		return False
 
+#switch is 1 by default, but changes to 0 when there are no brackets left.
+#then switch =0 ends the while loop
 switch = 1
 while switch == 1: 
 
-	for place in range(0,len(holder)): #Remember range doesn't include the upper bound!!!
+	for place in range(0,len(holder)): #range doesn't include the upper bound!
 		if  holder[place] == ')':
 			
-			#create an array containing the indices where '(' were found before ')'
+			#create an array of the indices where '(' are found before the current ')'
 			a = []	
-			for position in range(0,place-1): 
+			for position in range(0,place): 
 				if holder[position] == '(':
 					a.append(position)
 				else: 
@@ -73,7 +127,7 @@ while switch == 1:
 				
 				num1 = float(num1) #since abs only works on numbers
 				part = abs(num1)
-				print part
+				print part #our evaluation of what is in this part
 			
 			
 				
@@ -83,8 +137,7 @@ while switch == 1:
 				var_dict[temp_var] = temp_val
 				part = None
 			
-			#making the new array with one part calculated
-			
+			#making the new array without the part we just analyzed
 			how_far = place - biggest
 			how_far = int(how_far)
 				
@@ -93,9 +146,6 @@ while switch == 1:
 			if opp != 'define':
 				temp.append(part)
 			temp = temp + holder[biggest + how_far +1:]
-			print temp
-			
-			
 			holder = temp
 			break
 			
