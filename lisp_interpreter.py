bfa05ac93532b0399f9431168b1e7a807fc14671
Leta Montopoli <lmontopo@gmail.com>
2014-10-20 09:44:01 -0400

    Working interpreter that can interpret basic math, list, lambda and define inputs. Have not yet considered global vs non-global states.

--- lisp_interpreter.py
+++ lisp_interpreter.py
@@ -1,34 +1,35 @@
-holder = "(/ (+ 3  ( * 2 3 )) (- -4 2))"
+#holder = "((lambda (a b) (+ a b)) 5 6)"
 #holder = '(define x 3)(* (+ 3 2) x)'
+#holder = '((lambda (a) a) "blahblahblahblahblah")'
+#holder = '((define add3 (lambda (x) (+ x 3))) (add3 3))'
+#holder = '(+ 1 (abs -1))'
+holder = '(list 1 (+ 1 1) 3 4)'
 
+# ---- TOKENIZE ------ 
+holder = '(' + holder + ')'							#adding extra outer brackets
 holder = holder.replace( '(' , ' [ ' )
 holder = holder.replace( ')', ' ] ' )
-
 holder = holder.split(' ')
 holder = filter(lambda a: a != '', holder)
 print holder, 'before parsing'
-#def parse(list):
-#	if not list: 
-#		return
-#		raise ValueError('function being wierd')
-#	new_holder = []
-#	love = 0 
-#	for i in range(0,len(list)):
-#		if holder[i] == '[':
-#			love +=1
-#		elif holder[i] == ']':
-#			love -=1
-#		if love ==  0 :
-#			new_holder.append(list[i])
-#		elif love > 0:
-#			while love > 0:
-#				parse(new_holder)
-#	return new_holder		
-		
-#print parse(holder)
 
 
+# ----- USEFUL GLOBAL FUNCTIONS ----- 
+def check_type(x):
+	try:
+		return float(x)
+	except ValueError:
+		return str(x)
+			
+def is_number(x):
+	try:
+		float(x)
+		return True
+	except ValueError:
+		return False		
 
+
+# ---- PARSER ----- 
 def parse(holder):
 	if len(holder) == 0:
 		raise SyntaxError('unexpectedly entered function')
@@ -42,125 +43,103 @@ def parse(holder):
 		while holder[0] != ']':
 			to_append, holder = parse(holder)
 			new_holder.append(to_append)
-		holder = holder[1:]			 #pops off the ')' part
+		holder = holder[1:]							#pops off the ')' part
 		
 		#we add a condition to check if last return
 		if len(holder) > 0:
-			return new_holder , holder #if not last return, need to return current version of holder
+			return new_holder , holder 				#if not last return, need to return current version of holder
 		elif len(holder) == 0 :
-			return new_holder			#if last return, only want to return new_holder
+			return new_holder						#if last return, only want to return new_holder
 	else:
-		return token, holder 			#this is never last return, so we always need both
-
-		
-print parse(holder)
-
-
+		return check_type(token), holder 			#this is never last return, so we always need both
 	
+holder = parse(holder)
+print holder, 'after parsing'
 
-# a dictionary where we will add any variable declarations
+# --- DICTIONARY OF VARIABLES --- 
 var_dict = { }
-
-#checks if our operands are numbers
-def is_number(x):
-	try:
-		float(x)
-		return True
-	except ValueError:
-		return False
-
-#switch is 1 by default, but changes to 0 when there are no brackets left.
-#then switch =0 ends the while loop
-switch = 1
-while switch == 1: 
-
-	for place in range(0,len(holder)): #range doesn't include the upper bound!
-		if  holder[place] == ')':
-			
-			#create an array of the indices where '(' are found before the current ')'
-			a = []	
-			for position in range(0,place): 
-				if holder[position] == '(':
-					a.append(position)
-				else: 
-					pass
-			#finding the maximum element in list a
-			biggest = max(a)
-			
-			
-			opp = holder[biggest+1]
-			
-			
-			
-			if opp in ['+', '-', '/', '*']:
-				
-				if is_number(holder[biggest+2]) == True:
-					num1 = holder[biggest+2]
-				elif holder[biggest+2] in var_dict.keys():
-					num1 = var_dict[holder[biggest+2]]
-				else:
-					break
-					print "you have not defined holder[biggest+2]"
-				if is_number(holder[biggest+3]) == True:
-					num2 = holder[biggest+3]
-				elif holder[biggest+3] in var_dict.keys():
-					num2 = var_dict[holder[biggest+3]]
-				else:
-					break
-					print "you have not defined holder[biggest+3]"
-				
-				part = eval(num1 + opp + num2)
-				part = float(part) #So that i can divide and get 4.5 instead of 4
-				print part
-			
-			
-			
-			elif opp == 'abs':
-				
-				if is_number(holder[biggest+2]) == True:
-					num1 = holder[biggest+2]
-				elif holder[biggest+2] in var_dict.keys():
-					num1 = var_dict[holder[biggest+2]]
-				else:
-					break
-					print "you have not defined holder[biggest+2]"
-				
-				num1 = float(num1) #since abs only works on numbers
-				part = abs(num1)
-				print part #our evaluation of what is in this part
-			
-			
-				
-			elif opp == 'define':
-				temp_var = holder[biggest+2]
-				temp_val = holder[biggest+3]
-				var_dict[temp_var] = temp_val
-				part = None
-			
-			#making the new array without the part we just analyzed
-			how_far = place - biggest
-			how_far = int(how_far)
-				
-			part = str(part) # so that it can go back into the array with other string types
-			temp = holder[:biggest] 
-			if opp != 'define':
-				temp.append(part)
-			temp = temp + holder[biggest + how_far +1:]
-			holder = temp
-			break
-			
-	if not '(' in holder: 
-		switch = 0 
-		print "done"
-			 
-			
-			
-
-	
 	
 
 
-						
-		
-		
+# ----- INTERPRETER ------- 
+def evaluate(list_input):
+	
+	#base case
+	if not type(list_input) is list:
+		if is_number(list_input) == True:
+			return list_input
+		elif is_number(list_input) == False:
+			return var_dict[list_input]
+	
+	#evaluation
+	opp = list_input[0]
 
+	#removes any outermost brackets that are redundant
+	if type(opp) is list and len(list_input) == 1:
+		return evaluate(list_input[0])
+	
+	#---if operator is not list---
+	if not type(opp) is list:
+		
+		#basic math operations
+		if opp in ['+', '-', '*', '/']:
+			
+			#recursion	
+			result = eval(str(evaluate(list_input[1])) + opp + str(evaluate(list_input[2])))
+			return result
+			
+		#absolute value
+		if opp == 'abs':
+			if len(list_input) == 2:
+				return abs(evaluate(list_input[1]))
+			else:
+				raise SyntaxError('abs takes only one operand!')
+		
+		#lists
+		if opp == 'list':
+			python_list = []
+			list_size = len(list_input)
+			i = 1
+			while i < len(list_input):
+				item = evaluate(list_input[i])
+				python_list.append(item)
+				i += 1
+			return python_list
+			
+	
+		
+		#finding defined functions:
+		else: 
+			speed = var_dict[opp]
+			list_input[0] = speed
+			return evaluate(list_input)
+		
+	#---if operator is list----
+	if type(opp) is list:
+	
+		#handling lambdas
+		if opp[0] == 'lambda':
+			size_opp = len(opp[1])
+			size_list_input = len(list_input)-1
+			if size_opp != size_list_input:
+				raise SyntaxError("number of variables must = number of assignments")
+			i = 0
+			while i < size_opp:
+				what = list_input[i+1] #accessing variable assignments
+				what_2 = opp[1][i] #accessing variables
+				var_dict[what_2] = what
+				i += 1
+			return evaluate(opp[2])
+		
+		
+		#handling definitions
+		if opp[0] == 'define':
+			def_name = opp[1]
+			expression = opp[2]
+			var_dict[def_name] = expression
+			print var_dict, 'dictionnary'
+			list_input = list_input[1:]
+			return evaluate(list_input)
+
+		
+print evaluate(holder)
