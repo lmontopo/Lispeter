e59b24dbed3c0768de99948600b76edecae8e138
Leta Montopoli <lmontopo@gmail.com>
2014-10-21 17:20:21 -0400

    Able to interpret quote and set! commands. Still haven't dealt with global vs local variable issue.

--- lisp_interpreter.py
+++ lisp_interpreter.py
@@ -1,9 +1,19 @@
 #holder = "((lambda (a b) (+ a b)) 5 6)"
-#holder = '(define x 3)(* (+ 3 2) x)'
+#holder = '(define (func x)(+ x 2))(func 2)'
+#holder = '((( + 2 3 )))'
+#holder = '(define (func x)(+ x 2))'
 #holder = '((lambda (a) a) "blahblahblahblahblah")'
 #holder = '((define add3 (lambda (x) (+ x 3))) (add3 3))'
 #holder = '(+ 1 (abs -1))'
-holder = '(list 1 (+ 1 1) 3 4)'
+#holder = '(list 1 (+ 1 1) 3 4)'
+#holder = '(not (< 10 (- 20 10)))'
+#holder = "(if (not(< 10 20)) yes (+ 3 3))"
+#holder = '(if (< 10 20) ((lambda (a b) (+ a b)) 5 6) (+ 3 3))'
+#holder = "(cond ((> 0 0) 1)(else 'bloobloobloo))"
+#holder = '(quote hlallallaa)'
+holder = '(define x 3)(set! x (+ x 1))x'
+#holder = '(define (fib n)(cond ((= n 1) 0)((= n 2) 1)(else (+ (fib (- n 1)) (fib (- n 2))))))(fib 3'
+
 
 # ---- TOKENIZE ------ 
 holder = '(' + holder + ')'							#adding extra outer brackets
@@ -11,8 +21,7 @@ holder = holder.replace( '(' , ' [ ' )
 holder = holder.replace( ')', ' ] ' )
 holder = holder.split(' ')
 holder = filter(lambda a: a != '', holder)
-print holder, 'before parsing'
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 
 # ----- USEFUL GLOBAL FUNCTIONS ----- 
 def check_type(x):
@@ -35,6 +44,7 @@ def parse(holder):
 		raise SyntaxError('unexpectedly entered function')
 	
 	token = holder[0]
+
 	holder = holder[1:]
 
 	if token == '[':
@@ -53,47 +63,106 @@ def parse(holder):
 	else:
 		return check_type(token), holder 			#this is never last return, so we always need both
 	
+
 holder = parse(holder)
-print holder, 'after parsing'
+print holder, 'parsed holder'
+
+
 
 # --- DICTIONARY OF VARIABLES --- 
-var_dict = { }
+main_dict = { }
 	
 
 
 # ----- INTERPRETER ------- 
 def evaluate(list_input):
 	
+	print list_input, 'list_input'
+	
+	if list_input == []:
+		return None
+	
 	#base case
 	if not type(list_input) is list:
-		if is_number(list_input) == True:
+		if is_number(list_input) == True:			#number
 			return list_input
+		
 		elif is_number(list_input) == False:
-			return var_dict[list_input]
-	
+			if list_input[0] == "'":   				#quote
+				return list_input[1:]
+			elif list_input == 'else': 				#so that 'else' isn't evaluated
+				return False
+				
+			else:
+				try:
+					return main_dict[list_input]			#checks dict
+				except KeyError: 
+					return "inputed element is not number, not quote, and not in dict"
+			
+				
 	#evaluation
 	opp = list_input[0]
 
-	#removes any outermost brackets that are redundant
-	if type(opp) is list and len(list_input) == 1:
-		return evaluate(list_input[0])
 	
-	#---if operator is not list---
+	
+	#----if operator is not contained in another list---
 	if not type(opp) is list:
+	
+		#check if we've gotten final answer:
+		if is_number(opp) == True:
+			return opp
+			
 		
-		#basic math operations
-		if opp in ['+', '-', '*', '/']:
-			
-			#recursion	
-			result = eval(str(evaluate(list_input[1])) + opp + str(evaluate(list_input[2])))
+		#basic math and boolean operations
+		if opp in ['+', '-', '*', '/', '<', '>', '<=', '=','>=']:
+			if opp == '=':
+				result = eval(str(evaluate(list_input[1])) + '==' + str(evaluate(list_input[2])))
+			else:
+				result = eval(str(evaluate(list_input[1])) + opp + str(evaluate(list_input[2])))
 			return result
-			
+		
+		if opp == 'not':
+			result = eval(opp + ' ' + str(evaluate(list_input[1])))  #without the added space python sees 'notTrue' instead of 'not True'
+			return result
+		
+		
+		
+		#conditional statements	
+		if opp == 'if':
+			condition = list_input[1] 
+			consequence = list_input[2]
+			alt = list_input[3]
+			if evaluate(condition) == True:
+				return eval(str(evaluate(consequence)))
+			elif evaluate(condition) == False:
+				return eval(str(evaluate(alt)))
+		
+		if opp == 'cond':
+			for i in range(1, len(list_input)):
+				if evaluate(list_input[i][0]) == True:
+					return evaluate(list_input[i][1])
+				elif list_input[i][0] == 'else':
+					return evaluate(list_input[i][1])
+		
+		if opp == 'else': 
+			return evaluate(list_input[1])
+					
+				
+						
 		#absolute value
 		if opp == 'abs':
 			if len(list_input) == 2:
 				return abs(evaluate(list_input[1]))
 			else:
 				raise SyntaxError('abs takes only one operand!')
+		
+		
+		#quote
+		if opp == 'quote':
+			if len(list_input) == 2:
+				return list_input[1]
+			else:
+				raise SyntaxError('quote only takes one operand')
 		
 		#lists
 		if opp == 'list':
@@ -107,14 +176,15 @@ def evaluate(list_input):
 			return python_list
 			
 	
-		
-		#finding defined functions:
+	
+	
+		#finding defined functions in dictionary:
 		else: 
-			speed = var_dict[opp]
-			list_input[0] = speed
+			name = main_dict[opp]
+			list_input[0] = name
 			return evaluate(list_input)
 		
-	#---if operator is list----
+	#--- if operator is within another list----
 	if type(opp) is list:
 	
 		#handling lambdas
@@ -127,19 +197,65 @@ def evaluate(list_input):
 			while i < size_opp:
 				what = list_input[i+1] #accessing variable assignments
 				what_2 = opp[1][i] #accessing variables
-				var_dict[what_2] = what
+				main_dict[what_2] = what
 				i += 1
 			return evaluate(opp[2])
 		
 		
 		#handling definitions
 		if opp[0] == 'define':
-			def_name = opp[1]
-			expression = opp[2]
-			var_dict[def_name] = expression
-			print var_dict, 'dictionnary'
-			list_input = list_input[1:]
-			return evaluate(list_input)
+			if type(opp[1]) is list:
+				
+				if len(opp[1]) == 1:
+					def_name = opp[1][0]
+					expression = opp[2]
+					main_dict[def_name] = expression
+					list_input = list_input[1:]
+					return evaluate(list_input)
+				
+				
+				if len(opp[1]) > 1:
+					def_name = opp[1][0]
+					expression = opp[2]
+					dict = ['lambda']
+					dict_var = []
+					for i in range(1,len(opp[1])):
+						dict_var.append(opp[1][i])
+					dict.append(dict_var)
+					dict.append([expression])
+					main_dict[def_name] = dict
+					list_input = list_input[1:] #gets rid of entire define expression
+					return evaluate(list_input)
+			
+			elif not type(opp[1]) is list:
+				def_name = opp[1]
+				expression = opp[2]
+				main_dict[def_name] = expression
+				list_input = list_input[1:]
+				return evaluate(list_input)
+				
+		if opp[0] == 'set!':
+			print opp[0],'opp[0]'
+			#check already been defined:
+			if opp[1] in main_dict.keys():
+				expression = evaluate(opp[2])
+				main_dict[opp[1]] = expression
+				list_input = list_input[1:]
+				return evaluate(list_input)
+			else:
+				print "this should result in an error."
+			
+			
+			
+			
+			
+			
+		#removes any redundant outermost brackets 
+		if type(opp) is list and len(list_input) == 1:
+			return evaluate(list_input[0])
+		
 
+	
+	
 		
 print evaluate(holder)
